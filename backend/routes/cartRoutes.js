a1992a1caa0ee7043be330457a7229abeda13974
Sanket Jaswal <sanket.jaswal123@gmail.com>
2025-03-02 02:20:43 +0530

    cart routes fix

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,17 +1,16 @@
 import { Router } from "express";
 
-import { verifyToken } from "../middleware/authMiddleware.js";
-import { addProductToCart, removeProductFromCart } from "../controllers/cartController.js";
+import { addProductToCart, getCart, removeProductFromCart } from "../controllers/cartController.js";
 
 const cartRoutes = Router();
 
 // Add Product to Cart
-cartRoutes.post("/add", verifyToken, addProductToCart);
+cartRoutes.post("/add", addProductToCart);
 
 // Remove Product from Cart
-cartRoutes.delete("/remove/:productId", verifyToken, removeProductFromCart);
+cartRoutes.delete("/remove/:productId", removeProductFromCart);
 
 // Get User's Cart
-cartRoutes.get("/", verifyToken, getCart);
+cartRoutes.get("/", getCart);
 
 export default cartRoutes;
