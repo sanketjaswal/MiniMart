6e602fc7329e9fd0cef1cae564641c871b5d650c
Sanket Jaswal <sanket.jaswal123@gmail.com>
2025-03-04 13:17:12 +0530

    Api fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API = axios.create({ baseURL: "http://localhost:5000/api" });
+// export const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
-// export const API = axios.create({
-//     baseURL: "https://minimart-backend.onrender.com/api",
-//   });
+export const API = axios.create({
+    baseURL: "https://minimart-backend.onrender.com/api",
+  });
